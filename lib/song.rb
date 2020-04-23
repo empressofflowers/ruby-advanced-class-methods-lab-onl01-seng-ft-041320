caeca1c2e922d7c37da583b585be2fe1f65a3e15
Devone Austin <devone.austin@gmail.com>
2020-04-23 19:34:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,8 +38,8 @@ class Song
     self.find_by_name(name) || self.create_by_name(name)
   end
 
-  def self.alphabetical
-    all.sort
+  def self.alphabetical(name)
+    all.sort_by(name)
   end
 
   def self.destroy_all
