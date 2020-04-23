070c933048880ebe3826393ebf7da228a0903f21
Devone Austin <devone.austin@gmail.com>
2020-04-23 21:51:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,9 +50,10 @@ class Song
   end
 
   def self.create_from_filename(name)
-    self.new_from_filename.create
-    song.name = name
-    song
+    song = self.create
+    song.name = name.split(" - ")[1].chomp(".mp3")
+    song.artist_name = name.split(" - ")[0]
+    self.all << song
   end
 
   def self.destroy_all
